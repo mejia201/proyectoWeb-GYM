91a854f65e63b9e15ec92bf57fa8b57469548123
mejia201 <bryan.mejia@catolica.edu.sv>
2023-04-17 00:24:23 -0600

    Actualización de clases y vistas

---
+++ Models/Membresia.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace proyectoWeb_GYM.Models
+{
+    public class Membresia
+    {
+    public int id_membresia { get; set; }
+	public string nombre_membresia { get; set; }
+    public float precio { get; set; }
+	public int vendidas { get; set; }
+    }
+}
